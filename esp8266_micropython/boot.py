b623acc5f137b0f3814852f66b216a6231f7c961
necrosato <necrosato@live.com>
2018-03-02 11:11:06 -0800

    mpc working

---
+++ esp8266_micropython/boot.py
@@ -0,0 +1,16 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import gc
+import webrepl
+import network
+import esp
+esp.osdebug(None)
+webrepl.start()
+gc.collect()
+
+st_if = network.WLAN(network.STA_IF)
+ap_if = network.WLAN(network.AP_IF)
+
+ap_if.ifconfig(('6.6.6.1', '255.255.255.128', '6.6.6.1', '8.8.8.8'))
+
---
+++ esp8266_micropython/test.py
@@ -0,0 +1,5 @@
+print('Hello world! I can count to 10:')
+for i in range(1,11):
+    print(i)
+
+
