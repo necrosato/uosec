50e2762fd69e86c6872dcba8a04cfedbdccdbb7a
necrosato <necrosato@live.com>
2018-03-03 15:44:52 -0800

    buffer overflow test

--- buffer_overflow/unsecure_auth/main.c
+++ buffer_overflow/unsecure_auth/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
+char *pass = "security";
 
+struct bad_auth {
+    char buf[256];
+    unsigned int valid;
+};
 
+int main(int argc, char *argv[]) {
+    struct bad_auth password;
+    password.valid = 0;
+    printf("Password: ");
+    scanf("%s", password.buf);
+    if (strcmp(password.buf, pass) == 0) {
+        password.valid = 1;
+    }
+    if (password.valid) {
+        printf("Access Granted\n");
+    }
+    else {
+        printf("Access Denied\n");
+    }
+}
 
