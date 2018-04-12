0595352a2e8eb16c9c0c907e94338e343fbc43f4
necrosato <necrosato@live.com>
2018-04-12 12:29:24 -0700

    new stuff

---
+++ spring18/program.s
@@ -0,0 +1,44 @@
+	.file	"program.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	movl	$0, -8(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	-4(%rbp), %eax
+	addl	%eax, -8(%rbp)
+	addl	$1, -4(%rbp)
+.L2:
+	subl	$1000, -4(%rbp)
+	cmpl	$0, -4(%rbp)
+	jne	.L3
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 7.3.0-3) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits
